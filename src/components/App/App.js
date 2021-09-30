17c70236ace44250d2270f189f3caa6074974b8a
SarNadya <lutik.ne@gmail.com>
2021-09-30 14:26:12 +0300

    refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,10 +5,9 @@ import Footer from '../Footer/Footer';
 import ButtonAdd from '../ButtonAdd/ButtonAdd';
 import styles from './App.module.css';
 
-const todoItem = 'Поработать';
-
-const App = () => {
-  const items = [
+class App extends React.Component {
+  render() {
+    const items = [
     {
       value: 'Поработать',
       isDone: false
@@ -23,16 +22,18 @@ const App = () => {
     }
   ];
 
-  return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>Важные дела:</h1>
-      <div className={styles.button}>
-        <InputItem/>
-        <ButtonAdd/>
+    return (
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>Важные дела:</h1>
+        <div className={styles.button}>
+          <InputItem/>
+          <ButtonAdd/>
+        </div>
+        <ItemList items={items}/>
+        <Footer count={1}/>
       </div>
-      <ItemList items={items}/>
-      <Footer count={1}/>
-    </div>);
+    );
+  }
 }
 
 export default App;
