a26f5328e985c6fef0d61ebc955b77de19dfb5e7
SarNadya <lutik.ne@gmail.com>
2021-10-02 21:37:21 +0300

    revision

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import classnames from 'classnames';
 import styles from './Item.module.css';
+import CheckboxLabel from '../CheckboxLabel/CheckboxLabel';
+import ButtonDelete from '../ButtonDelete/ButtonDelete';
 
-const Item = ({value, isDone,}) => (
-  <span className={
-  classnames({
-    [styles.item]: true,
-    [styles.done]: isDone
-  })
-}>
-  {value}
-</span>);
+const Item = ({value, isDone, onClickDone}) => (
+  <div className={styles.list}>
+    <CheckboxLabel isDone={isDone} onClickDone={onClickDone}/>
+    <span className={
+      classnames({
+        [styles.item]: true,
+        [styles.done]: isDone
+      })
+    }
+      onClick={() => onClickDone(isDone)}
+    >
+      {value}
+    </span>
+    <ButtonDelete/>
+  </div>);
 
 export default Item;
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Item from '../Item/Item';
-import ButtonDelete from '../ButtonDelete/ButtonDelete';
-import CheckboxLabel from '../CheckboxLabel/CheckboxLabel';
 import styles from './ItemList.module.css';
 
 const ItemList = ({items, onClickDone}) => (<ul className={styles.list}>
-  {items.map(item => <li className={styles.button} key={item.value}>
-    <CheckboxLabel onClickDone={onClickDone}/>
+  {items.map(item => <li key={item.value}>
     <Item value={item.value} isDone={item.isDone} onClickDone={onClickDone}/>
-    <ButtonDelete/>
   </li>)}
 </ul>);
 
